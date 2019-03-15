a12449286b0756fb08046fe97f27e861d7c77a22
Ripal Patel <ripal212007@gmail.com>
2019-03-14 22:16:40 -0500

    Add project files.

---
+++ PlanetsLINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlanetsLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
